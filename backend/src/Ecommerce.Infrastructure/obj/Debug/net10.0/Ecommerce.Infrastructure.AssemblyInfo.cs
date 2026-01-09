fab045d422b7d356ce0ec6bae831e41f8aa9ce64
davitvarshanidze <davittvarshanidze@gmail.com>
2026-01-09 18:33:33 +0400

    Add ef core + postgresql

--- backend/src/Ecommerce.Infrastructure/obj/Debug/net10.0/Ecommerce.Infrastructure.AssemblyInfo.cs
+++ backend/src/Ecommerce.Infrastructure/obj/Debug/net10.0/Ecommerce.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06f3b3e7d2ea84f6e8077a6f3216d4535252bbaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
