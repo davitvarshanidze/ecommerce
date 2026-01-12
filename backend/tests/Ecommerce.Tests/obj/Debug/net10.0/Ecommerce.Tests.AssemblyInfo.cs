ab8368d48cb7b39901d4e9b3a17c0f45142ff162
davitvarshanidze <davittvarshanidze@gmail.com>
2026-01-12 11:39:52 +0400

    Add NuGet packages

--- backend/tests/Ecommerce.Tests/obj/Debug/net10.0/Ecommerce.Tests.AssemblyInfo.cs
+++ backend/tests/Ecommerce.Tests/obj/Debug/net10.0/Ecommerce.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63dfebfa6378761a1b205e3e3112c8506ccfa6d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+839c89d2633986e3a63d65e5c7e62f7bff67587a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
